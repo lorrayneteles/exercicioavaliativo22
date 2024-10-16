a73b86a915de51d492a3837dab41fe43b143cdca
lorrayneteles <lorrayneteles1305@gmail.com>
2024-10-16 18:58:37 -0300

    Add files via upload

---
+++ exercicioavaliaivo22.py
@@ -0,0 +1,6 @@
+#divisao inteira de dois numeros sem considerar o resto
+
+num1 = int(input('Digite um numero:'))
+num2 = int(input('Digite outro numero:'))
+divisao = num1 // num2
+print(f'A divisao inteira de {num1} por {num2} é {divisao}')  
